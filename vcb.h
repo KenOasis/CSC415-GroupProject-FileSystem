2b18be99d98b0051e081ee45d586009de23544f2
Student <csitu1@mail.sfsu.edu>
2021-04-14 22:23:12 -0700

    fixed

--- vcb.h
+++ vcb.h
@@ -2,8 +2,8 @@ typedef struct vcb {
     int number_of_blocks;  // number of blocks in volume
     int size_of_block;  // the size of a single block
     int total_size_in_bytes;  // the product of number_of_blocks and size_of_block
-    int LBA_free_space;  // the LBA of free space
-    int LBA_root_directory;  // the LBA of the root directory
+    uint64_t LBA_free_space;  // the LBA of free space
+    uint64_t LBA_root_directory;  // the LBA of the root directory
     int magic_number;  // magic number: where to write what value?
     int number_of_free_blocks;  // number of free blocks
     freespace* free_space_ptr;  // not persistent
