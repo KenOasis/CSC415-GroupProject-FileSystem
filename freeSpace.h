b8c37f4dba8b24ece35b4186b6fd33b8fe094f03
mikeyhwang <myhwang43@gmail.com>
2021-05-06 16:52:43 -0700

    Up to date with dev-linux-yunhao

--- freeSpace.h
+++ freeSpace.h
@@ -2,15 +2,20 @@
 #define _FREESPACE_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include "fsLow.h"
+
 typedef struct  {
 	int* bitVector;
-	int nextFreePosition;
-	int nextFreeIndex;
 	int size;
+	uint64_t LBABitVector; //LBA location of bit vector
+	int blocksNeeded;
+	int structSize;
+	int blockCount; //amount of blocks vector is accounting for
 } freeSpace;
 
-freeSpace* init_freeSpace(int totalBlocks, int bytesPerBlock);
-int findFreeBlock(freeSpace* vector);
-int findMultipleBlocks(int blockCount, freeSpace* vector);
-
+freeSpace* init_freeSpace(int totalBlocks, int bytesPerBlock); //initialize the free space if it doesn't exist
+u_int64_t findMultipleBlocks(int blockCount); //find free blocks and return the LBA
+void freeSomeBits(int startIndex, int count); //free a chunk of blocks
+u_int64_t expandFreeSection(int fileLocation, int fileBlockSize, int newBlockSize); //expand the file to new size, move file if current location can't be allocated
 #endif
