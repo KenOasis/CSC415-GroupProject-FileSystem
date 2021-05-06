2283b195ed35d09e7e1eb14bd3bf6b30f9c0bcfd
mikeyhwang <myhwang43@gmail.com>
2021-05-06 14:52:51 -0700

    changed freeSpace.c

--- freeSpace.c
+++ freeSpace.c
@@ -1,12 +1,19 @@
 #include "freeSpace.h"
+extern freeSpace* vector;
 
 freeSpace* init_freeSpace(int totalBlocks, int BytesPerBlock) {
-	freeSpace *vector = malloc(sizeof(freeSpace));
+	vector = malloc(512);
+	vector->LBABitVector = ((sizeof(freeSpace) + 511) / 512) + 2; //size of freeSpace struct without the array
 	int integers = (totalBlocks + 31) / 32; //how many integers in the int array
-	int blocksNeeded = ((integers * 32) + BytesPerBlock * 8 - 1) / (BytesPerBlock * 8); //amount of BITS needed for free space bit vector
-	blocksNeeded += 1; //add VCB needed block
+	//printf("integers needed: %d\n", integers);
+	int blocksNeeded = ((integers * 32) + BytesPerBlock * 8 - 1) / (BytesPerBlock * 8); //amount of blocks needed for free space bit vector
+	blocksNeeded += vector->LBABitVector; //add VCB needed block, and freeSpace struct needed blocks
+	//printf("blocks needed: %d\n", blocksNeeded);
 	vector->size = integers; //length of the bit vector for printing or iterating
+	vector->blocksNeeded = (integers + 127) / 128;
+	vector->blocksNeeded += vector->LBABitVector;
 	vector->bitVector = malloc(sizeof(int) * integers);
+	vector->structSize = 512; //size of freeSpace guaranteed to be less than 1 block
         for (int b = 0; b < integers; b++) {
                 vector->bitVector[b] = 0; //sets all of the free space array to 0
         }
@@ -16,39 +23,75 @@ freeSpace* init_freeSpace(int totalBlocks, int BytesPerBlock) {
 		flag = flag << n;
 		vector->bitVector[0] |= flag;
 	}
-	vector->nextFreePosition = 0;
-	vector->nextFreeIndex = blocksNeeded; //next free block is right after free space bit vector
+	//vector->nextFreePosition = 0;
+	//vector->nextFreeIndex = blocksNeeded; //next free block is right after free space bit vector
 	return vector;
 }
 
-int findFreeBlock(freeSpace* vector) {
-	int freeIndex = vector->nextFreeIndex + vector->nextFreePosition * 32; //the bit number that is free, aka the next free block
-	vector->bitVector[vector->nextFreePosition] |= 1 << vector->nextFreeIndex; //sets bit to 1 for future reference
-	vector->nextFreeIndex += 1;
-	if (vector->nextFreeIndex >= 32) { //if nextFreeIndex >= 32, then move to next int in the bit vector
-		vector->nextFreePosition += 1;
-		vector->nextFreeIndex = 0;
+u_int64_t findMultipleBlocks(int blockCount) {
+	int consecutiveFree = 0; 						//tracks how many free spaces have been found in a row
+	int freeIndex = 0; 							//the bit index that is free, followed by more free space
+	for (int n = 0; n < vector->size; n++) { 		//iterates through the ints in the bitvector
+		for (int a = 0; a < 32; a++) { 				//iterates through the bits in bitvector
+			if ((vector->bitVector[n] & (1 << a)) == 0) {		//if vector bit is 0 aka free
+				if (consecutiveFree == 0) {					//checks if this is the first free bit found, or following free bits
+					freeIndex = n * 32 + a;
+					consecutiveFree += 1;
+					//printf("FREE BLOCKS werE FOUND\n");
+				} else {
+					consecutiveFree += 1;
+				}
+			} else {								//chain of free bits not big enough
+				consecutiveFree = 0;
+				freeIndex = 0;
+			}
+			if (consecutiveFree >= blockCount) {	//found the necessary chunk, sets all used bits to 1 and return the index of first free bit
+				int count = consecutiveFree;
+				int position = freeIndex;
+				int freePos = position / 32;
+				int freeBit = position % 32;
+				while (count > 0) {
+					vector->bitVector[freePos] |= 1 << freeBit;
+					count -= 1;
+					freeBit += 1;
+					if (freeBit >= 32) {
+						freePos += 1;
+						freeBit = 0;
+					}
+				}
+				LBAwrite(vector->bitVector, (vector->size + 127) / 128, vector->LBABitVector); //write the updates to bit vector to LBA
+				return freeIndex;
+			}
+		}
 	}
-	if (vector->nextFreePosition == vector->size) { //freeSpace is full
-		return -1;
-	}
-	return freeIndex;
+	freeIndex = 0;
+	return freeIndex; 								//bits were not found, return 0 to indicate error
 }
 
-int findMultipleBlocks(int blockCount, freeSpace* vector) {
-	int count = blockCount; //tracks how many remaining blocks to reserve/handle
-	int freeIndex = vector->nextFreeIndex + vector->nextFreePosition * 32; //the bit index that is free, followed by more free space
-	while (count > 0) { //counts down and manipulates the bits that are free that will be used, returns -1 if not enough bits/blocks left
-		vector->bitVector[vector->nextFreePosition] |= 1 << vector->nextFreeIndex; //set bit to 1 for future reference
-		vector->nextFreeIndex += 1;
-		if (vector->nextFreeIndex >= 32) { //if nextFreeIndex >= 32, then move to next int in the bit vector
-                	vector->nextFreePosition += 1;
-                	vector->nextFreeIndex = 0;
-        	}
-		if (vector->nextFreePosition == vector->size) { //free space is full
-			return -1;
+void freeSomeBits(int startIndex, int count) {
+	int index = startIndex / 32;
+	int position = startIndex % 32;
+	for (int n = 0; n < count; n++) { //iterates through bits to be freed
+		int value = 1 << position;
+		value = ~value;
+		vector->bitVector[index] &= value;
+		position += 1;
+		if (position >= 32) {
+			index += 1;
+			position = 0;
 		}
-		count--;
 	}
-	return freeIndex;
+	LBAwrite(vector->bitVector, (vector->size + 127) / 128, vector->LBABitVector); //write the updates to bit vector to LBA
+}
+
+u_int64_t expandFreeSection(int fileLocation, int fileBlockSize, int newBlockSize) {
+	freeSomeBits(fileLocation, fileBlockSize); //frees the original bits
+	u_int64_t newLBA = findMultipleBlocks(newBlockSize); //looks for the new size; if new size won't fit, it will find the proper block, otherwise it'll just find the same
+	if (newLBA > 0) { //moves the data in the old location into the new LBA
+		char * buf = malloc(4096 * fileBlockSize);
+		LBAread(buf, fileBlockSize, fileLocation);
+		LBAwrite(buf, fileBlockSize, newLBA);
+		free(buf);
+	}
+	return newLBA;
 }
